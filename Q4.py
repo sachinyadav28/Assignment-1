eac01743e221baf2a07bbf0e5f7d97a49d474ef4
sachinyadav28 <58584293+sachinyadav28@users.noreply.github.com>
2019-12-09 13:20:47 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,3 +1,13 @@
+#Given the following two sets find the intersection and remove those elements from the
+#first set.
+#Given:
+#>> First Set {65, 42, 78, 83, 23, 57, 29}
+#>> Second Set {67, 73, 43, 48, 83, 57, 29}
+#Expected Outcome:
+#>> Intersection is {57, 83, 29}
+#>> First Set after removing common element {65, 42, 78, 23}
+
+
 set1  = {23, 42, 65, 57, 78, 83, 29}
 set2 = {57, 83, 29, 67, 73, 43, 48}
 
