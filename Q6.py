c8b428760b00a467c43c0fe92aab67fa241c94b6
sachinyadav28 <58584293+sachinyadav28@users.noreply.github.com>
2019-12-09 13:23:03 +0530

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -1,3 +1,13 @@
+#Remove duplicate from a list and create a tuple and find the min and max number.
+#Given:
+#sampleList = [87, 45, 41, 65, 94, 41, 99, 94]
+#Expected Outcome:
+#>> unique items [87, 45, 41, 65, 99]
+#tuple (87, 45, 41, 65, 99)
+#min: 41
+#max: 99
+
+
 sampleList = [87, 45, 41, 65, 94, 41, 99, 94]
 
 uniqueitems = list(dict.fromkeys(sampleList))
