c5a5df167d96dc7360c8bcff35ed05a82ab51b0a
sachinyadav28 <58584293+sachinyadav28@users.noreply.github.com>
2019-12-09 13:18:47 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,3 +1,9 @@
+#Write a Python program that accepts a comma-separated sequence of words as input
+#and prints the words in a comma-separated sequence after sorting them alphabetically.
+#Suppose the following input is supplied to the program:
+#>> orange,apple,mango,grapes
+#Then, the output should be:
+#>> apple,grapes,mango,orange
 x = input("Enter fruits with comma separated ")
 y = x.split(",")
 y = sorted(y)
