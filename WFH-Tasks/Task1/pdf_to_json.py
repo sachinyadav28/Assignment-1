d1bc9589107a29c7ca39837b0f0e6c9667f47c5d
sachinyadav28 <sachinyadav@qainfotech.com>
2020-04-03 18:10:44 +0530

    added json file and added json writer in method

--- WFH-Tasks/Task1/pdf_to_json.py
+++ WFH-Tasks/Task1/pdf_to_json.py
@@ -4,7 +4,7 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdftypes import resolve1
 
-def pdf_form_to_json(pdf_path):
+def pdf_form_to_json(pdf_path, json_path):
     fp = open(pdf_path, 'rb')
     parser = PDFParser(fp)
     doc = PDFDocument(parser)
@@ -12,15 +12,24 @@ def pdf_form_to_json(pdf_path):
     page_count = resolve1(doc.catalog['Pages'])['Count']
     field_count = 0
     form_data = {"document.pagecount":page_count}
+
     for i in fields:
         field = resolve1(i)
         field_count += 1
         name, value = field.get('T'), field.get('V')
         form_data[name] = value
+        
     form_data["document.fieldcount"] =  field_count
     form_json = json.dumps({"map":coercion.normalize_collection(form_data)})
-    return form_json
 
-pdf_file = '/home/sachinyadav/Documents/Task1/Enrollment_Form_filled.pdf'
-print(pdf_form_to_json(pdf_file))
+    with open(json_path, 'w') as files:
+        files.write(form_json)
+
+    print("Success")
+
+pdf_file = 'Enrollment_Form_filled.pdf'
+json_file = 'Enrollment_Form.json'
+
+pdf_form_to_json(pdf_file,json_file)
+
 
